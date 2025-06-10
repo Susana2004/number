acc5c80beb8f1b773bda9ba34119cb36d9d487ab
Susana2004 <gs6986164@gmail.com>
2025-06-10 13:00:02 +0530

    Save it

---
+++ numbers.java
@@ -0,0 +1,8 @@
+public class numbers{
+    public static void main(String[] args){
+        int num = 5;
+        if(num > 0){
+            System.out.println("NUMBER IS POSITIVE");
+        }
+    }
+}
